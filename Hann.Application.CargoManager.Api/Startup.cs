bdff9f60c1d6531eada77de512dabb50ff7c279b
Cyprian Okeke <onwudiweokeke@gmail.com>
2022-02-01 12:39:36 -0800

    remove comment

--- Hann.Application.CargoManager.Api/Startup.cs
+++ Hann.Application.CargoManager.Api/Startup.cs
@@ -21,7 +21,7 @@ namespace Hann.Application.CargoManager.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             var conntionStrings = Configuration.GetConnectionString("CargoRequestConnectionString");
